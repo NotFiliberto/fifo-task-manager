88790d3e56506aba000e3998272b57262915f920
Filiberto <filiberto.dev@gmail.com>
2022-12-24 10:27:27 +0100

    clean example of task manager initialization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { TaskManager, CustomTask } from "./classes"
+import path from "path"
 
 async function main() {
-    const taskManager = new TaskManager<CustomTask>("./src/worker.ts", 2, [
+    const WORKER_PATH = path.resolve(__dirname, "./worker.ts")
+    const MAX_TASKS_IN_PARALLEL = 2
+    const tasks = [
         new CustomTask("mario"),
         new CustomTask("doccia"),
         /* new CustomTask("11111"),
@@ -10,7 +13,13 @@ async function main() {
         new CustomTask("44444"),
         new CustomTask("55555"),
         new CustomTask("lokita"), */
-    ])
+    ]
+
+    const taskManager = new TaskManager<CustomTask>(
+        WORKER_PATH,
+        MAX_TASKS_IN_PARALLEL,
+        tasks
+    )
 
     setTimeout(() => taskManager.addTask(new CustomTask("homyatol")), 11000) //send after 2 seconds
     /* setTimeout(() => {

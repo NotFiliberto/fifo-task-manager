55d4b61b05905ac0965e520a5c0bb1e3fb46898d
Filiberto <filiberto.dev@gmail.com>
2022-12-24 10:28:07 +0100

    documentation

--- src/classes/Task.ts
+++ src/classes/Task.ts
@@ -1,15 +1,43 @@
+/**
+ * Generic task, you have to override run method, and if you need extra data during the execution save it in data property of Task class
+ */
 export default abstract class Task {
+    /**
+     * Local data of the task
+     *
+     * @protected
+     * @type {*}
+     * @memberof Task
+     */
     protected data: any
 
+    /**
+     * Deep recursive clone of data passed to the constructor
+     * @param data data to store in the task
+     */
     constructor(data?: any) {
         if (data) this.data = structuredClone(data)
         else data = {}
     }
+
+    /**
+     *
+     * @returns task's data
+     */
     public getData() {
         return this.data
     }
+
+    /**
+     *
+     * @param data data to store in the task
+     */
     public setData(data: any) {
         this.data = structuredClone(data)
     }
+    /**
+     * Method to run the task, you have to OVERRIDE it
+     * @param args any argouments to the funciont
+     */
     abstract run(...args: any): any
 }
